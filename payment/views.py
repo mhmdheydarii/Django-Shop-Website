dea08828a1de2bd626d707ce574ed05148011cec
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-06-14 14:24:38 -0700

    make better paymeny view

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.shortcuts import get_object_or_404
+from django.db import transaction
 from django.urls import reverse_lazy
 from .models import PaymentModel, PaymentStatusType
 from .zarinpall_client import ZarinPalSandbox
@@ -14,7 +15,7 @@ class PaymentVerifyView(View):
     def get(self, request, *args, **kwargs):
         authority_id = request.GET.get("Authority")
         payment_obj = get_object_or_404(PaymentModel, authority_id=authority_id)
-        order = OrderModel.objects.get(payment=payment_obj)()
+        order = OrderModel.objects.get(payment=payment_obj)
         zarinpal = ZarinPalSandbox()
         response = zarinpal.payment_verify(
             int(payment_obj.amount), payment_obj.authority_id
@@ -23,6 +24,24 @@ class PaymentVerifyView(View):
         data = response.get('data', {})
 
         if data.get("code") in [100, 101]:
+
+            if payment_obj.status == PaymentStatusType.success.value:
+                return render(request, "order/completed.html")
+
+            with transaction.atomic():
+
+                for item in order.order_items.all():
+
+                    product = ProductModel.objects.select_for_update().get(
+                        id=item.product.id
+                    )
+
+                    if product.stock < item.quantity:
+                        return render(request, "order/failed.html")
+
+                    product.stock -= item.quantity
+                    product.save()
+
             payment_obj.ref_id = data.get("ref_id")
             payment_obj.response_code = data.get("code")
             payment_obj.status = PaymentStatusType.success.value
