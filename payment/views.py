d9c7afe2644edff92b2c5f49ddd9026750f95999
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-06-14 13:58:20 -0700

    fixed the payment app

--- payment/views.py
+++ payment/views.py
@@ -5,32 +5,42 @@ from django.urls import reverse_lazy
 from .models import PaymentModel, PaymentStatusType
 from .zarinpall_client import ZarinPalSandbox
 from order.models import StatusTypeModel, OrderModel
+from shop.models import ProductModel
 # Create your views here.
+
 
 class PaymentVerifyView(View):
 
     def get(self, request, *args, **kwargs):
         authority_id = request.GET.get("Authority")
-        status = request.GET.get("Status")
         payment_obj = get_object_or_404(PaymentModel, authority_id=authority_id)
-        order = OrderModel.objects.get(payment=payment_obj)
+        order = OrderModel.objects.get(payment=payment_obj)()
         zarinpal = ZarinPalSandbox()
-        response = zarinpal.payment_verify(int(payment_obj.amount), payment_obj.authority_id)
-        if response["Status"] == 100 or response["Status"] == 101:
-            payment_obj.ref_id = response["RefID"]
-            payment_obj.response_code = response["Status"]
+        response = zarinpal.payment_verify(
+            int(payment_obj.amount), payment_obj.authority_id
+        )
+
+        data = response.get('data', {})
+
+        if data.get("code") in [100, 101]:
+            payment_obj.ref_id = data.get("ref_id")
+            payment_obj.response_code = data.get("code")
             payment_obj.status = PaymentStatusType.success.value
             payment_obj.response_json = response
             payment_obj.save()
+
             order.status = StatusTypeModel.success.value
             order.save()
-            return render(reverse_lazy("order:completed"))
+
+            return render(request, "order/completed.html")
         else:
-            payment_obj.ref_id = response["RefID"]
-            payment_obj.response_code = response["Status"]
+            payment_obj.ref_id = data.get("ref_id")
+            payment_obj.response_code = data.get("code")
             payment_obj.status = PaymentStatusType.failed.value
             payment_obj.response_json = response
+            payment_obj.save()
+
             order.status = StatusTypeModel.failed.value
             order.save()
-            return render(reverse_lazy("order:failed"))
-    
+
+            return render(request, "order/failed.html")
